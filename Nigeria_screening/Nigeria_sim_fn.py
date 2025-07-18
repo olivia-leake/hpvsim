96f8d22e0c0b8f7f8e749a747e68595a7ce4a8c1
olivia-leake <olivialeake2003@gmail.com>
2025-07-18 11:53:26 +0100

    Update Nigeria_sim_fn.py

--- Nigeria_screening/Nigeria_sim_fn.py
+++ Nigeria_screening/Nigeria_sim_fn.py
@@ -106,7 +106,7 @@ def make_nigeria_sim(interventions=None, rand_seed=None,vax=True, **kwargs):
 
 
 # Works alone
-sim1 = make_nigeria_sim(label = 'no vax')
+sim1 = make_nigeria_sim(label = 'no vax 9 to 10, vax 9 to 14')
 sim1.run()
 sim1.plot()
 
@@ -123,12 +123,14 @@ sim3['location'] # india
 
 
 # Accepts interventions
-vx = hpv.routine_vx(prob=0.6, start_year=2015, age_range=[9,10], product='bivalent')
-sim4 = make_nigeria_sim(interventions = [vx], label = 'vax')
+vx1 = hpv.routine_vx(prob=0.6, start_year=2015, age_range=[9,10], product='bivalent')
+sim4 = make_nigeria_sim(interventions = [vx1], label = 'vax 9 to 10 and vax 9 to 14')
 sim4.run()
 sim4.plot()
 sim4['interventions'] 
-# Out[328]: [hpv.routine_vx(product=bivalent, prob=None, age_range=[9, 10], sex=0, eligibility=None, label=None)]
+# Out[134]: 
+# [hpv.routine_vx(product=quadrivalent, prob=None, age_range=[9, 14], sex=0, eligibility=None, label=None),
+#  hpv.routine_vx(product=bivalent, prob=None, age_range=[9, 10], sex=0, eligibility=None, label=None)]
 # Not sure why prob = None
 # That said, if you compare it to Sim1, cancer incidence per age is the same up to 2015,
 # and declines after that, therefore am sure that it is being applied
@@ -143,7 +145,7 @@ sim5.plot()
 
 
 # Plot comparison 
-msim.plot(sim1, sim4) # note this doesn't work because it only works if you used
+# msim.plot(sim1, sim4) # note this doesn't work because it only works if you used
 # multisim to create the plots in the first place
 
 # You don't need to run them again, but you do have to create a multisim in order
@@ -153,6 +155,17 @@ msim = hpv.MultiSim([sim1,sim4])
 
 msim.plot()
 
+# Now let's compare both sim1 and sim4 to themselves but with the vax in 2024 removed
+# sim1 no vax
+sim1_nv =make_nigeria_sim(vax=False, label = 'no vax at all')
+sim1_nv.run()
+sim4_nv = make_nigeria_sim(vax=False, interventions = [vx1], label = 'vax 9 to 10 and no vax 9 to 14')
+sim4_nv.run()
 
+msim_comp = hpv.MultiSim([sim1, sim1_nv, sim4, sim4_nv]) # I forgot to put the sims as a list and it didn't work !
+msim_comp.plot()
+
+# I think the issue is when I put vax = False, I can't then override it. It's not adding vx1
+# But then why are sim1 and sim4 not being plot?
 
 
